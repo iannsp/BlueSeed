a24a0458e021fb6b9afedfd7aa0f415c9c2e3b0f
Ivo Nascimento <iannsp@gmail.com>
2011-07-26 01:21:05 -0300

    Alteracoes de configuração

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -4,8 +4,6 @@ define( 'APP_PATH', dirname(__FILE__).'/' );
 
 require_once 'config/system.conf.php';
 
-parse_ini_file('config/db.conf');
-
 function _autoload_($name) {
 	if(file_exists(APP_PATH."class/{$name}.php")){
         require_once(APP_PATH."class/{$name}.php");
@@ -16,7 +14,13 @@ function _autoload_($name) {
     else if(file_exists(INTERFACE_PATH."{$name}.php")){
         require_once(INTERFACE_PATH."{$name}.php");
     }
+    else if(file_exists(VO_PATH."{$name}.php")){
+        require_once(VO_PATH."{$name}.php");
+    }
 }
 spl_autoload_register('_autoload_');
 $systimezone= new DateTimeZone('America/Sao_Paulo');
-Registry::RegistryVar('timezone', $systimezone);
+Registry::setVar('timezone', $systimezone);
+
+$database = Database::getInstance();
+$database->loadfromConf(APP_PATH.'config/db.conf');
