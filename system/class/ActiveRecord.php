044aa76c86a10f048508c621dedeb12a81196c50
Ivo Nascimento <iannsp@gmail.com>
2011-07-29 13:34:17 -0300

    Alteração para use de prepare leitura de metadados dos objetos direto da anotação

--- system/class/ActiveRecord.php
+++ system/class/ActiveRecord.php
@@ -3,44 +3,40 @@
 abstract class ActiveRecord{
 	private $fields = Array();
 	private $values = Array();
-	private $type;
-	private $tableName;
-	private $indexName;
-	private $connectionName;
+	private $type; 
+	private $sa;
 	private function loadMeta(){
-		if ($this->type)
+		if ( count($this->fields) )
 			return;
 		$rInstance = new ReflectionClass($this);
-		$ba		   = New SystemAnnotation( $rInstance->getName() );
+		$this->sa		   = New SystemAnnotation( $rInstance->getName() );
 		$properties = $rInstance->GetProperties(ReflectionProperty::IS_PUBLIC);
 		foreach ($properties as $name => $property){
-			array_push($this->fields, $property->getName() );
-			array_push($this->values, ( is_null( $value = $property->getValue( $this ) ) )?'default':$value );
+			array_push($this->fields, ($fname= $property->getName()) );
+			array_push($this->values, $this->$fname )  ;
 		}
 		$this->type				= 	$rInstance->getName();
-		$this->tableName		= 	$ba->get('@tableName');
-		$this->indexName		= 	$ba->get('@indexName');
-		$this->connectionName	= 	$ba->get('@connectionName');
-		
 	}
 	public function getIndexName(){
 		$this->loadMeta();
-		return $this->indexName;
+		return $this->sa->get('@indexName');
 	}
 	public function getIndexValue(){
-		$idxn = $this->indexName;
+		$this->loadMeta();
+		$idxn = $this->getIndexName();
 		return $this->$idxn;
 	}
 	
 	public function getTableName(){
 		$this->loadMeta();
-		return $this->tableName;
+		return $this->sa->get('@tableName');
 	}
 	public function getConnectionName(){
 		$this->loadMeta();
-		return $this->connectionName;
+		return $this->sa->get('@connectionName');
 	}
 	private function setIndexValue($value){
+		$this->loadMeta();
 		$idxname 		= $this->getIndexName();
 		$this->$idxname = $value;
 	}
@@ -51,47 +47,55 @@ abstract class ActiveRecord{
 		$result = Search::Select($vo)->Where()->equal($vo->getIndexName())->exec();
 		return (count($result)==1)?$result[0]:NULL;
 	}
-	public static function select(){
+	public static function search(){
 //		Search::Select( )
 	}
 	public function save(){
 		$this->loadMeta();
-		if (is_null($this->getIndexValue()))
-			$this->insert();
+		if (is_null($this->getIndexValue())){
+			$id = $this->insert();
+			$this->setIndexValue($id);
+		}
 		else
 			$this->update();
 	}
 	private function update(){
 		$updateTerm = Array();
-		foreach ($this->fields as $idx => $value){
-			if ($value != $this->getIndexName() )
-				array_push ($updateTerm, "{$value}= '{$this->values[$idx]}'");
+		foreach ($this->fields as $idx => $field){
+			if ($field != $this->getIndexName() )
+				array_push ($updateTerm, "{$field}= :{$field}");
 		}
 		$updatestr =  implode(",",$updateTerm);
-		$sql = "update {$this->getTableName()} set {$updatestr} where {$this->getIndexName()} = '{$this->getIndexValue()}';";
-		$ret =  Database::getInstance()->
-		get( $this->getConnectionName() )->
-		get()->Exec($sql);
-		return $ret;
-		
+		$stmt = Database::getInstance()->get( $this->getConnectionName() )->get()->prepare(
+			"update {$this->getTableName()} set {$updatestr} where {$this->getIndexName()} = '{$this->getIndexValue()}';"
+		);
+		foreach ($this->fields as $idx => $field){
+			if ($field != $this->getIndexName() )
+			$stmt->bindParam(":{$field}", $this->values[$idx]);
+		}
+		$stmt->execute();
 	}
 	private function insert(){
-		$sql = "insert into {$this->getTableName()} (".implode(",",$this->fields).") values(".implode(",'",$this->values)."');";
-		echo $sql;
-		Database::getInstance()->
-		get( $this->getConnectionName() )->
-		get()->Exec($sql);
+		$stmt = Database::getInstance()->get( $this->getConnectionName() )->get()->prepare(
+			"insert into {$this->getTableName()} (".implode(",",$this->fields).") values(:".implode(",:",$this->fields).");"
+		);
+		foreach ($this->fields as $idx => $field){
+			$stmt->bindParam(":{$field}", $this->values[$idx]);
+		}
+		$stmt->execute();
 		return Database::getInstance()->
-		get( $this->getConnectionName() )->
-		get()->lastInsertId("{$this->getTableName()}_{$this->getIndexName()}_seq");
+						get( $this->getConnectionName() )->
+						get()->
+						lastInsertId("{$this->getTableName()}_{$this->getIndexName()}_seq");
 	}
 	public function delete(){
 		$this->loadMeta();
-		$sql = "delete from {$this->getTableName()} where {$this->getIndexName()} = '{$this->getIndexValue()}';";
-		Database::getInstance()->
-		get( $this->getConnectionName() )->
-		get()->Exec($sql);
-		$idxname = $this->getIndexName();
-		$this->$idxname = null;
+		$stmt = Database::getInstance()->get( $this->getConnectionName() )->get()->prepare(
+			"delete from {$this->getTableName()} where {$this->getIndexName()} = :{$this->getIndexName()};"
+		);
+		$value = $this->getIndexValue();
+		$stmt->bindParam(":{$this->getIndexName()}", $value );
+		$stmt->execute();
+		$this->setIndexValue(null);
 	}
 }
