be916e4ac79a6aedb5d04579cb2fe4338f07b818
Ivo Nascimento <iannsp@gmail.com>
2011-08-12 01:12:51 -0300

    sample using ENCODING application constant
    You can use any encoding, just set it in application config options and
    set the header with this value. Can use this same value to inform
    incoming in html code

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 include '../Application/bootstrap.php';
+if (isset(ENCODING)){
+        header('Content-type: text/html; charset='.ENCODING);
+}
 $C = new BlueSeed\ApplicationController();
 $C->dispatch();
 
