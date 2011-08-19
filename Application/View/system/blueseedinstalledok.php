41d7546e5e9eefa66b8298efde4cebf9fa567434
Er Galvão Abbott <galvao@galvao.eti.br>
2011-08-18 21:40:41 -0300

    Erro na definição da propriedade color, havia um '0' sobrando. Erro na definição da propriedade target da tag a: "blanck" -> "blank"

--- Application/View/system/blueseedinstalledok.php
+++ Application/View/system/blueseedinstalledok.php
@@ -1,7 +1,7 @@
 <html>
-<body style='background-color: #99CCFF O' >
+<body style='background-color: #99CCFF'>
 <div style='width:100%; margin-top:70px;font-family:sans-serif;font-weight: bolder;' align='center'>
- <a href='http://ianntech.com.br/blue-seed' target="_blanck"><img src='/images/blueseed-2.png'><br/></a>
+ <a href='http://ianntech.com.br/blue-seed' target="_blank"><img src='/images/blueseed-2.png'><br/></a>
 <br/>
 Instalado com sucesso.
 </div>
