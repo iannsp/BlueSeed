d25b0c53d0f04a29edef774d66eed88b7ca04401
Your Name <iannsp@gmail.com>
2011-08-31 12:08:11 +0000

    When loadformConf method are use its create the PDO instances to use as parameter to DatabaseConnection Construction

--- BlueSeed/Database.php
+++ BlueSeed/Database.php
@@ -2,7 +2,7 @@
 namespace BlueSeed;
 
 /**
- * 
+ *
  * Administrate the DatabaseConnectionCollection
  * @author ivonascimento <ivo@o8o.com.br>
  * @license   http://www.opensource.org/licenses/bsd-license.php BSD
@@ -11,21 +11,21 @@ namespace BlueSeed;
  */
 class Database {
 	/**
-	 * 
+	 *
 	 * the static instance of Database for the Singleton
 	 * @var Database
 	 * @access private
 	 */
 	private static $instance;
 	/**
-	 * 
+	 *
 	 * The DatabaseCollection where any DatabaseConnection are stored
 	 * @var DatabaseCollection
 	 * @access private
 	 */
 	private $databaseCollection;
 	/**
-	 * 
+	 *
 	 * the Constructor for Database Instance
 	 * @access private
 	 * @return void
@@ -34,18 +34,18 @@ class Database {
 		$this->databaseCollection = new DatabaseCollection();
 	}
 	/**
-	 * 
-	 * The Singloton request method 
+	 *
+	 * The Singloton request method
 	 * @return Database
 	 * @access public
 	 */
 	public static function getInstance(){
 		if (is_null(self::$instance ) )
 			self::$instance = new Database();
-		return self::$instance;		
+		return self::$instance;
 	}
 	/**
-	 * 
+	 *
 	 * The method to add DatabaseConnection items in DatabaseCollection
 	 * @param String $name the label for request a especific connection
 	 * @param DatabaseConnection $dbconn
@@ -56,7 +56,7 @@ class Database {
 		$this->databaseCollection[ $name ] = $dbconn;
 	}
 	/**
-	 * 
+	 *
 	 * use this method to retrieve a DatabaseConnection by your label/name
 	 * @param String $name
 	 * @throws Exception
@@ -70,29 +70,28 @@ class Database {
 			foreach ($this->databaseCollection  as $item)
 				return $item;
 		}
-		else 
+		else
 			throw New \Exception("Necessario indicar um nome de Item");
 	}
 	/**
-	 * 
-	 * This method are used to load DatabaseConnections 
+	 *
+	 * This method are used to load DatabaseConnections
 	 * configurations from a ini file in Blue Seed sintax
 	 * @param String $confpath
 	 * @throws Exception
 	 * @access public
 	 * @return void
-	 */	
+	 */
 	public function loadfromConf($confpath){
 		$dbitens = parse_ini_file($confpath, true);
 		foreach ($dbitens as $idx => $each){
 			try{
 				$dsn = "{$each['driver']}:host={$each['host']};dbname={$each['database']};port={$each['port']}";
-				$this->addDatabase($idx, new DatabaseConnection( $dsn, $each['user'], $each['password']) );
+				$this->addDatabase($idx, new DatabaseConnection( new \Pdo( trim($dsn), trim($each['user']), trim($each['password']))) );
 			}catch(\Exception $Exception){
 				throw new \Exception("Problemas na conexao com o DB {$idx} ({$dsn}) [ {$Exception->getMessage()} ]" );
-			}			
+			}
 		}
-				
 	}
 }
 
