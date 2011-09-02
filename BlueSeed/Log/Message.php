cb3258bf5489827c21576c1d5567f31a02d6af55
Your Name <iannsp@gmail.com>
2011-09-02 12:03:42 +0000

    the abstract class to support basic operations in Log (you log a message concrete Object)

---
+++ BlueSeed/Log/Message.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author iann
+ * this class format a Message to Use in Writers
+ *
+ */
+namespace BlueSeed\Log;
+abstract class Message
+{
+	/**
+	 *
+	 * make the properties in a string when use this class in echo
+	 */
+	public function __toString()
+    {
+    	$props 	= Array();
+    	$properties = $this->getProperties();
+		foreach ($properties as  $property) {
+			$props .= array_push($props, "{$property->getName()}:{$property->getValue()}");
+		}
+		return explode(',', $props);
+    }
+	/**
+	 *
+	 * Explicit request of all data into String
+	 */
+    public function toString()
+    {
+    	return $this->__toString();
+    }
+
+    /**
+     *
+     * Return all Properties in a Json type
+     */
+    public function toJson()
+    {
+		return json_encode($this);
+    }
+
+    /**
+     *
+     * return Properties Objects with all private property(log value)
+     */
+    protected function getProperties()
+    {
+		$class 		= New \ReflectionClass($this);
+		return $class->getProperties(\ReflectionProperty::IS_PRIVATE);
+    }
+}
+?>
