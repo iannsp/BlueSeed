c785d84ae317271b2b5c66dbbe8481ef05c2fbcc
Your Name <iannsp@gmail.com>
2011-11-10 15:12:16 +0000

    fix directory_separator

--- BlueSeed/bootstrap.php
+++ BlueSeed/bootstrap.php
@@ -3,13 +3,11 @@ define( 'SYS_PATH', dirname(__FILE__) );
 $pathinfo 	=	pathinfo(SYS_PATH);
 
 require_once SYS_PATH.'/Config/system.conf.php';
-
-function _autoload_($name) {
+function _bsautoload_($name) {
 	global $pathinfo;
-
 	$searchpath 	= 	explode('\\', $name);
 	$name	= array_pop( $searchpath );
-	$searchpath =  $pathinfo['dirname']."/".implode('/',$searchpath)."/";
+	$searchpath =  $pathinfo['dirname'].DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR,$searchpath).DIRECTORY_SEPARATOR;
 	try{
 		if(file_exists("{$searchpath}{$name}.php")){
 	        require_once("{$searchpath}{$name}.php");
@@ -19,4 +17,4 @@ function _autoload_($name) {
 	}
 }
 
-spl_autoload_register('_autoload_');
+spl_autoload_register('_bsautoload_');
