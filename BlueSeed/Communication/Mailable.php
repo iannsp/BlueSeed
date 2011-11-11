b581cd0166538a4b8c2a4ab907de4adb7d3621ea
Your Name <iannsp@gmail.com>
2011-11-11 12:53:37 +0000

    a simple interface provide better conditions to select a email object

---
+++ BlueSeed/Communication/Mailable.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *
+ */
+interface Mailable {
+	/**
+	 * Send a Mail
+	 * @param string $to
+	 * @param array $header
+	 * @param string $body
+	 * @return boolean
+	 */
+	public function send( $to, array $header, $body);
+}
+
+?>
