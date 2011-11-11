d46740ff248b31b767338a9bfaf99d7edd7acee9
Your Name <iannsp@gmail.com>
2011-11-11 13:03:19 +0000

    start using mailable

--- BlueSeed/Email.php
+++ BlueSeed/Email.php
@@ -1,56 +1,60 @@
 <?php
 namespace BlueSeed;
 
+
 /**
- * 
+ *
  * The controller to support interpretation of requests
  * @author ivonascimento <ivo@o8o.com.br>
  * @license   http://www.opensource.org/licenses/bsd-license.php BSD
  * @package system
- * 
+ *
  */
+
+use BlueSeed\Communication\Mailable;
 
 class Email {
 	/**
-	 * 
-	 * the Mail Pear Instance 
-	 * @var Mail
+	 *
+	 * the Mailable InstanceMailable
+	 * @var Mailable
 	 */
 	private $mailmethod;
 	/**
-	 * 
+	 *
 	 * the Message Body
 	 * @var string
 	 */
 	private $body;
 	/**
-	 * 
+	 *
 	 * Mail Subject
 	 * @var string
 	 */
 	private $subject;
 	/**
-	 * 
-	 * the e-mail of sender 
+	 *
+	 * the e-mail of sender
 	 * @var string
 	 */
 	private $from;
 	/**
-	 * 
+	 *
 	 * the e-mail to (can by maore then one)
 	 * @var string
 	 */
 	private $to;
 	/**
-	 * 
+	 *
 	 * Constructor where the Mail Pear are Instanciate
+	 * @param Maileble $mailObject
 	 * @return void
 	 */
-	public function __construct(){
-		//$this->mailmethod = Mail::factory("mail");
+	public function __construct(Mailable $mailObject){
+		$this->mailmethod = $mailObject;
 	}
 	/**
-	 * 
+	 *
 	 * setter to Body
 	 * @param string
 	 * @return void
@@ -59,8 +63,8 @@ class Email {
 		$this->body = $str;
 		return $this;
 	}
-	/**
-	 * 
+	/**Mailable
+	 *
 	 * setter to Subject
 	 * @param string $str
 	 * @return void
@@ -70,7 +74,7 @@ class Email {
 		return $this;
 	}
 	/**
-	 * 
+	 *
 	 * setter to From
 	 * @param string $mailfrom
 	 * @return void
@@ -80,7 +84,7 @@ class Email {
 		return $this;
 	}
 	/**
-	 * 
+	 *
 	 * setter to TO
 	 * @param string $mailto
 	 * @return void
@@ -90,27 +94,27 @@ class Email {
 		return $this;
 	}
 	/**
-	 * 
+	 *
 	 * Send the email
 	 * @param boolean $asHTML
 	 * @return void
 	 */
 	public function send($asHTML = true){
 		if ( !($this->body && $this->from && $this->subject && $this->to) )
-			throw  New \Exception ("Falha ao enviar e-mail. Os dados n‹o foram informados.");
+			throw  New \Exception ("Falha ao enviar e-mail. Os dados nÃ£o foram informados.");
 		$headers = array("From"=>$this->from, "Subject"=>$this->subject );
 		if ($asHTML){
 			$headers['Content-type'] =  'text/html; charset=iso-UTF-8';
 		}
 		try{
-			$this->mailmethod->send( $this->to, $headers, $this->body);
+			$return = $this->mailmethod->send( $this->to, $headers, $this->body);
 		}catch ( \Exception $E){
 			throw New \Exception("Falha ao enviar e-mail usando Pear/Mail");
 		}
-			
+
 	}
 	/**
-	 * 
+	 *
 	 * reset the data to prepare a New Mail if all data is newer
 	 * @return void
 	 */
@@ -120,7 +124,7 @@ class Email {
 		$this->from			= "";
 		$this->to			= "";
 	}
-	
+
 }
 
 ?>
