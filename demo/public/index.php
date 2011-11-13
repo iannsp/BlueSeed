e75674d9ed384e77b4ebcbfbc83d4f5cff49e3cb
Ivo Nascimento <iannsp@gmail.com>
2011-11-12 23:51:52 -0200

    get out demo to redesign

--- demo/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../Application/bootstrap.php';
-if (ENCODING){
-        header('Content-type: text/html; charset='.ENCODING);
-}
-$C = new BlueSeed\ApplicationController();
-$C->dispatch();
-
