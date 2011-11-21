1ed13c50f3a8a364f448df794625a537772388a2
Your Name <iannsp@gmail.com>
2011-11-21 20:03:44 +0000

    add parameter to method and get out make method

--- Crud/Crud.php
+++ Crud/Crud.php
@@ -1,38 +1,50 @@
 <?php
 namespace BlueSeed\Crud;
+use BlueSeed\ActiveRecord;
+use BlueSeed\Request;
 /**
  * @author iann
  *
  *
  */
 class Crud implements CrudInterface {
+	/**
+	 *
+	 * the ActiveRecord Data
+	 * @var ActiveRecord
+	 */
 	private $dataObject;
-	private $request;
-	public function __construct(ActiveRecord $dataObject,Request $request)
+
+	public function __construct(ActiveRecord $dataObject)
 	{
 		$this->dataObject 	= $dataObject;
-		$this->request		= $request;
 	}
-	public function create	()
+	public function create	(Array $postData)
 	{
-
+		$idxname	= $this->dataObject->getIndexName();
+		$meta		= $this->dataObject->getMeta();
+        foreach ($postData as $k => $datum) {
+        	if ($k == $idxname )
+        		continue;
+        	$this->dataObject	= $datum;
+        }
+        $this->dataObject->Save();
 	}
-	public function read	()
+	public function read	(Array $findData)
 	{
-
+		$meta		= $this->dataObject->getMeta();
+		$fields = Array();
+		foreach ($meta->fields as $field) {
+			$fields[ $field ] = $this->dataObject->$field;
+		}
+		return	$fields;
 	}
-	public function update	()
+	public function update	(Array $postData)
 	{
-
+		$this->create();
 	}
-	public function delete	()
+	public function delete	(Array $findData)
 	{
-
-	}
-	public function make()
-	{
-
+        $this->dataObject->delete();
 	}
 }
-
-?>
