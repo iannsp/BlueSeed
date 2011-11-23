aa8e2eb6ae82934889d246184a76feef45f7e19d
Your Name <iannsp@gmail.com>
2011-11-23 16:39:44 +0000

    lot of changes as temp

--- Scaffold/Scaffold.php
+++ Scaffold/Scaffold.php
@@ -1,5 +1,5 @@
 <?php
-namespace BlueSeed;
+namespace BlueSeed\Scaffold;
 /**
  * @author iann
  *
@@ -12,61 +12,82 @@ use BlueSeed\Crud\Crud;
 class Scaffold {
 	private $objData;
 	private $request;
-	function __construct(ActiveRecord $objData, Request $request)
+	private $template;
+	function __construct(ActiveRecord $objData, Request $request, Template $tmpl)
 	{
 		$this->objData	= $objData;
 		$this->request	= $request;
+		$this->template = $tmpl;
 	}
-	public static function creator(ActiveRecord $objData, Request $request)
+	public static function creator(ActiveRecord $objData, Request $request, Template $tmpl)
 	{
-		return New Scaffold($objData, $request);
+		return New Scaffold($objData, $request, $tmpl);
 	}
 	public function create()
 	{
-
 		$crud = New Crud($this->objData);
 		$crud->create($this->request->getParams());
 	}
 	public function read()
 	{
+		$template = $this->template;
+		$indexname = $this->objData->getIndexName();
+		if (!is_null($this->objData->$indexname)) {
+			$template->setTemplateObject($this->objData);
+			$template->setData(Array($this->objData));
+			$template->form();
+		}else{
+			$template->setTemplateObject($this->objData);
+			$template->setData($this->objData->fetchAll());
+			$template->index();
+		}
 		$crud = New Crud($this->objData);
-		$crud->read();
+		$crud->read(Array());
 
 	}
 	public function update()
 	{
-
+		echo "Em Atualizar<br/>";
+		$crud = New Crud($this->objData);
+//		var_dump($this->request->getParams());
+		$crud->update($this->request->getParams());
+		$indexname 	= $this->objData->getIndexName();
+		$this->objData->$indexname = null;
+		return $this->read();
 	}
 	public function delete()
 	{
-
+		echo "Em Apagar<br/>";
+		$crud = New Crud($this->objData);
+		$crud->delete($this->request->getParams());
+		$indexname 	= $this->objData->getIndexName();
+		$this->objData->$indexname = null;
+		return $this->read();
 	}
 	public function make()
 	{
-		$indexname = $this->objData->getIndexName();
+		$indexname 	= $this->objData->getIndexName();
+		$idxval 	= $this->request->getQuery(2);
+		$idxname 	= $this->request->getQuery(1);
 
-		if (!is_null($this->request->getQuery($indexname))){
-			$this->objData = $this->objData->find(
-				$this->request->getQuery($indexname)
-			);
-		if ($this->request->isPost()){
-			// bssop BlueSeed(bs) Scaffold (s) Operation (op)
-			if(!is_null($this->request->getParam('bssop'))){
-				$this->delete();
-			}else {
-				$this->create();
-			}
-		}else {
+		if (!is_null($idxval)){
+			$this->objData = $this->objData->find($idxval);
 		}
-				$this->read();
-				$this->update();
+		if ($this->request->isPost()){
+			// bssop BlueSeed(bs) Scaffold (s) Operation (op) // a button
+			if(!is_null($this->request->getParam('bssop'))) {
 				$this->delete();
-			}else{
-				$this->read();
-
+			}else if ($this->request->getParam($indexname)!=""){
+				return $this->update();
+			}else {
+				return $this->create();
 			}
+		}else if (!is_null($idxval)){
+			return $this->read();
+		}else {
+			return $this->read();
+		}
 
-		var_dump($this);
 	}
 }
 
