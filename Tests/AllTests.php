ed92f364b65674191ed7f979fdae1ee19c43c5a6
Ivo Nascimento <iannsp@gmail.com>
2011-12-06 01:22:51 -0200

    update the phpunit Suite

--- Tests/AllTests.php
+++ Tests/AllTests.php
@@ -1,25 +1,27 @@
 <?php
 
 require_once 'PHPUnit/Framework/TestSuite.php';
-
+require_once 'RequestTest.php';
+require_once '../bootstrap.php';
 /**
  * Static test suite.
  */
-class testSuite extends PHPUnit_Framework_TestSuite {
+class BlueSeedSuite extends PHPUnit_Framework_TestSuite {
 
     /**
      * Constructs the test suite handler.
      */
     public function __construct() {
-        $this->setName ( 'BlueSeedTests' );
-
-        $this->addTestSuite ( 'ViewTest' );
-
+        $this->setName ( 'BlueSeedSuite' );
+        $this->addTestSuite('RequestTest');
     }
+    protected function setUp()
+        {
+        }
 
-    /**
-     * Creates the suite.
-     */
+    protected function tearDown()
+    {
+    }
     public static function suite() {
         return new self ();
     }
