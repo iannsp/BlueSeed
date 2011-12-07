ea278be86126825a222f344bdcec9b974b7ac3c0
Your Name <iannsp@gmail.com>
2011-12-07 18:11:41 +0000

    start using ACL into Crud

--- Crud/Crud.php
+++ Crud/Crud.php
@@ -1,5 +1,9 @@
 <?php
 namespace BlueSeed\Crud;
+use BLueSeed\Acl\CrudAcl;
+
+use BlueSeeed\Acl\AccessControllable;
+
 use BlueSeed\ActiveRecord;
 use BlueSeed\Request;
 /**
@@ -14,13 +18,22 @@ class Crud implements CrudInterface {
 	 * @var ActiveRecord
 	 */
 	private $dataObject;
+	private $ACL;
 
 	public function __construct(ActiveRecord $dataObject)
 	{
 		$this->dataObject 	= $dataObject;
+		$this->ACL			= $acl;
+	}
+	public function attachAcl(AccessControllable $acl){
+		$this->ACL = $acl;
+		return $this;
 	}
 	public function create	(Array $postData)
 	{
+		if (!is_null($this->ACL) and !$this->ACL->can(CrudAcl::CREATE)){
+			return false;
+		}
 		$idxname	= $this->dataObject->getIndexName();
 		$meta		= $this->dataObject->getMeta();
         foreach ($postData as $k => $datum) {
@@ -32,6 +45,9 @@ class Crud implements CrudInterface {
 	}
 	public function read	(Array $findData)
 	{
+		if (!is_null($this->ACL) and !$this->ACL->can(CrudAcl::RETRIEVE)){
+			return Array();
+		}
 		$meta		= $this->dataObject->getMeta();
 		$fields = Array();
 		foreach ($meta->fields as $field) {
@@ -41,10 +57,15 @@ class Crud implements CrudInterface {
 	}
 	public function update	(Array $postData)
 	{
-		$this->create($postData);
+		if (!is_null($this->ACL) and !$this->ACL->can(CrudAcl::UPDATE)){
+			$this->create($postData);
+		}
 	}
 	public function delete	(Array $findData)
 	{
+		if (!is_null($this->ACL) and !$this->ACL->can(CrudAcl::DELETE)){
+			return false;
+		}
 		$idxname	= $this->dataObject->getIndexName();
 		$meta		= $this->dataObject->getMeta();
 		if (array_key_exists($idxname, $findData)) {
